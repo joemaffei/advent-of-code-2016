56959c5dd4cfaf25bef9357f7edd2488f2f18c79
joemaffei <joemaffei@users.noreply.github.com>
2016-12-07 21:54:52 -0600

    day07-part01 WIP replaced [a-z] with (.)

--- day07-part01.js
+++ day07-part01.js
@@ -1,5 +1,5 @@
 // WIP: close, but it still captures every ABBA but the last one in brackets
-var regex = /([a-z])([a-z])\2\1(?=\[)|([a-z])([a-z])\4\3(?!\])/g
+var regex = /(.)(.)\2\1(?=\[)|(.)(.)\4\3(?!\])/g
 
 // abba[mnop]qrst[boob]poop   //passes
 // abcd[bddb]xyyx             //passes
